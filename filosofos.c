186e7448811af67b45ceb88fec3d4fc49ac96452
JENIFFER MOREIRA BORGES <JENIFFER.BORGES@edu.pucrs.br>
2022-04-06 20:20:54 -0300

    com fontes

--- filosofos.c
+++ filosofos.c
@@ -4,6 +4,9 @@
                 Code, Compile, Run and Debug C program online.
 Write your code in this editor and press "Run" button to compile and execute it.
 
+https://www.revista-programar.info/artigos/threads-semaforos-e-deadlocks-o-jantar-dos-filosofos/
+
+
 *******************************************************************************/
 
 #include<stdio.h>
